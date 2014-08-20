c8dcb38b5cb56e43724fdb92682c779ec263744b
Anvar Azizov <anvardejim@gmail.com>
2014-08-20 15:40:34 +0300

    3-way quick sort method added but now I testing it

--- AlgorithmsAndDataStructures/SuffixArray.m
+++ AlgorithmsAndDataStructures/SuffixArray.m
@@ -50,4 +50,66 @@
 	return text;
 }
 
+#pragma mark – 3-way sorting methods
+
+- (void)threeWaySortArray:(NSMutableArray *)array
+{
+	NSInteger leftIndex = 1;
+	NSInteger rightIndex = array.count - 1;
+	[self sortArray:array withLeft:leftIndex andRight:rightIndex];
+}
+
+- (void)sortArray:(NSMutableArray *)array withLeft:(NSInteger )left andRight:(NSInteger )right
+{
+	NSInteger i = left - 1;
+	NSInteger j = right;
+	NSInteger p = left - 1;
+	NSInteger q = right;
+	
+	if (right <= left)
+		return;
+	
+	id v = array[right];
+	
+	for (; ;)
+	{
+		while ([array[++i] compare:v] == NSOrderedAscending);
+		while ([v compare:array[--j]] == NSOrderedAscending)
+			if (j == left) break;
+		if (i >= j) break;
+		
+		[array exchangeObjectAtIndex:i withObjectAtIndex:j];
+		
+		if ([array[i] compare:v] == NSOrderedSame)
+		{
+			p++;
+			[array exchangeObjectAtIndex:p withObjectAtIndex:i];
+		}
+		
+		if ([v compare:array[j]] == NSOrderedSame)
+		{
+			q--;
+			[array exchangeObjectAtIndex:j withObjectAtIndex:q];
+		}
+	}
+	
+	[array exchangeObjectAtIndex:i withObjectAtIndex:right];
+	
+	j = i - 1;
+	i = i + 1;
+	
+	for (int k = left; k < p; k++)
+	{
+		[array exchangeObjectAtIndex:k withObjectAtIndex:j--];
+	}
+	
+	for (int k = right - 1; k > q; k--, i++)
+	{
+		[array exchangeObjectAtIndex:i withObjectAtIndex:k];
+	}
+	
+	[self sortArray:array withLeft:left andRight:j];
+	[self sortArray:array withLeft:i andRight:right];
+}
+
 @end
